66913814b9b7f7812c3d55b3830b549d647b6251
MuskanH <132813639+MuskanH@users.noreply.github.com>
2024-04-25 21:04:51 +0530

    Update Portfolio.jsx

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -3,6 +3,7 @@ import "./Portfolio.css"
 import IMG1 from "../../assets/Frame1.png"
 import IMG2 from "../../assets/Frame2.png"
 import IMG3 from "../../assets/Frame3.png"
+import IMG4 from "../../assets/Frame4.png"
 
 const Portfolio = () => {
   return (
@@ -41,6 +42,17 @@ const Portfolio = () => {
         <div className='portfolio__item-cta'>
         <a href='https://github.com/MuskanH/Spotify-clone' className='btn' target='_blank'>GitHub</a>
         <a href='https://www.linkedin.com/posts/muskan-chavda-7308a31a3_webdev-frontend-spotify-activity-7098703162643021824-nKdA?utm_source=share&utm_medium=member_desktop' className='btn btn-primary' target='_blank'>Live Demo</a>
+        </div>
+       </article>
+
+         <article className='portfolio__item'>
+        <div className='portfolio__item-image'>
+         <img src={IMG4}/>
+        </div>
+        <h3>E-commerce website</h3>
+        <div className='portfolio__item-cta'>
+       
+        <a href='https://www.kavimports.com/' className='btn btn-primary' target='_blank'>Live Demo</a>
         </div>
        </article>
       </div>
