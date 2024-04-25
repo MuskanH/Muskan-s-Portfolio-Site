7b7db5d08c7ec402561c23a18e67974cbe0c3708
MuskanH <132813639+MuskanH@users.noreply.github.com>
2024-04-25 20:50:04 +0530

    Update CTA.jsx

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from "../../assets/Muskan Frontend Dev Resume.pdf"
 const CTA = () => {
   return (
     <div className='CTA'>
-      <a href= {CV} className='btn'>Download CV</a>
+      <a href= "https://docs.google.com/document/d/1q1In6F-C8o8zH1u7yRG-YCKGVFI7Eu7ZoBuSKwHvQTo/edit?usp=sharing" className='btn'>View CV</a>
       <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
