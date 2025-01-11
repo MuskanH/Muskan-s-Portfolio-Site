03cc21796fda33ad4627cfe1399b14f8cc2cacee
MuskanPruthatek <muskan@pruthatek.com>
2025-01-11 16:27:38 +0530

    Projects added

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from "../../assets/Muskan Frontend Dev Resume.pdf"
 const CTA = () => {
   return (
     <div className='CTA'>
-      <a href= "https://drive.google.com/file/d/1iiuMI_u3fa6oOxeK9YViqm4mePcf1F_s/view?usp=sharing" className='btn'>View CV</a>
+      <a href= "https://drive.google.com/file/d/1WMihYSEcLeECoQFOHWjt7Lz8KPFgOQYg/view?usp=sharing" target='_blank' className='btn'>View CV</a>
       <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,6 +4,8 @@ import IMG1 from "../../assets/Frame1.png"
 import IMG2 from "../../assets/Frame2.png"
 import IMG3 from "../../assets/Frame3.png"
 import IMG4 from "../../assets/Frame 4.png"
+import IMG5 from "../../assets/Frame 5.png"
+import IMG6 from "../../assets/Frame 6.png"
 
 const Portfolio = () => {
   return (
@@ -12,6 +14,51 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className='container portfolio__container'>
+
+      <article className='portfolio__item'>
+        <div className='portfolio__item-image'>
+         <img src= {IMG5}/>
+        </div>
+        <h3>Radiant Planks</h3>
+        <div className='portfolio__item-cta'>
+        {/* <a href='https://github.com/MuskanH/Wanderlust-project' className='btn' target='_blank'>GitHub</a> */}
+        <a href='https://radiantplanks.com/' className='btn btn-primary' target='_blank'>Live Demo</a>
+        </div>
+       </article>
+
+       <article className='portfolio__item'>
+        <div className='portfolio__item-image'>
+         <img src= {IMG5}/>
+        </div>
+        <h3>Radiant Planks CRM</h3>
+        <div className='portfolio__item-cta'>
+        {/* <a href='https://github.com/MuskanH/Wanderlust-project' className='btn' target='_blank'>GitHub</a> */}
+        <a href='https://www.linkedin.com/posts/muskan-chavda-7308a31a3_inventory-management-and-crm-software-for-activity-7283772764241481728-MsJn?utm_source=social_share_sheet&utm_medium=member_desktop_web' className='btn btn-primary' target='_blank'>Live Demo</a>
+        </div>
+       </article>
+
+       <article className='portfolio__item'>
+        <div className='portfolio__item-image'>
+         <img src= {IMG6}/>
+        </div>
+        <h3>DR. CRM</h3>
+        <div className='portfolio__item-cta'>
+        {/* <a href='https://github.com/MuskanH/Wanderlust-project' className='btn' target='_blank'>GitHub</a> */}
+        <a href='https://www.linkedin.com/posts/muskan-chavda-7308a31a3_crm-application-for-doctors-key-features-activity-7283784449492725760-PQbW?utm_source=share&utm_medium=member_desktop' className='btn btn-primary' target='_blank'>Live Demo</a>
+        </div>
+       </article>
+
+       <article className='portfolio__item'>
+        <div className='portfolio__item-image'>
+         <img src={IMG4}/>
+        </div>
+        <h3>E-commerce website</h3>
+        <div className='portfolio__item-cta'>
+       
+        <a href='https://www.kavimports.com/' className='btn btn-primary' target='_blank'>Live Demo</a>
+        </div>
+       </article>
+
        <article className='portfolio__item'>
         <div className='portfolio__item-image'>
          <img src= {IMG1}/>
@@ -45,16 +92,7 @@ const Portfolio = () => {
         </div>
        </article>
 
-         <article className='portfolio__item'>
-        <div className='portfolio__item-image'>
-         <img src={IMG4}/>
-        </div>
-        <h3>E-commerce website</h3>
-        <div className='portfolio__item-cta'>
-       
-        <a href='https://www.kavimports.com/' className='btn btn-primary' target='_blank'>Live Demo</a>
-        </div>
-       </article>
+
       </div>
     </section>
   )
