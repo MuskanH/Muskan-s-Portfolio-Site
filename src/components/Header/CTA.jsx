34108548ea20b49d708850fe473e0a9ee60f9d2b
MuskanPruthatek <muskan@pruthatek.com>
2025-07-21 19:35:43 +0530

    cv added

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from "../../assets/Muskan Frontend Dev Resume.pdf"
 const CTA = () => {
   return (
     <div className='CTA'>
-      <a href= "https://drive.google.com/file/d/1860P8KrT-2mLPflrGB_Xbmvxunke4dIk/view?usp=sharing" target='_blank' className='btn'>View CV</a>
+      <a href= "https://drive.google.com/file/d/1Af3L71z6AC32cUApbd4bN0tr-aBTP9QE/view?usp=sharing" target='_blank' className='btn'>View CV</a>
       <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
